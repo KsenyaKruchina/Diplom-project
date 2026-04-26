9dc73f0306c431e985c0baa9c7b7dd306aafb079
Kseniya <kseniya@MacBook-Pro-Kseniya.local>
2026-04-26 19:58:12 +0500

    Удалены отдельные Icons

--- frontend/src/components/icons/Description.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export const Description = ({ className }) => (
-    <svg className={className} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path d="M14 2H6c-1.1 0-2 .9-2 2v16c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V8l-6-6zm2 18H6V4h7v5h5v11zM8 10h8v2H8zm0 4h8v2H8zm0 4h8v2H8z" fill="currentColor"/>
-  </svg>
-);
